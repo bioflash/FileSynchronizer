a518abe21eac54f5709d67c2fc378d2e513e27d6
bioflash <bioflash@gmail.com>
2016-01-12 18:20:49 +0800

    hide identicial files from compare result by default

--- client/index.js
+++ client/index.js
@@ -28,7 +28,7 @@ fileSyncApp.factory('applyChangeSvc', ['$http', ($http)=>{
 
 fileSyncApp.controller('mainController', ['$scope','findDifferenceSvc','applyChangeSvc',($scope,findDifferenceSvc,applyChangeSvc)=>{
     $scope.diffs = [];
-
+    $scope.hideEqualFiles = true;
     $scope.compare = function(){
         if (!$scope.sourceDir||!$scope.targetDir){
             alert('Source and Target dir can not be empty')
@@ -55,6 +55,14 @@ fileSyncApp.controller('mainController', ['$scope','findDifferenceSvc','applyCha
         }
     }
 
+    $scope.hideEquals = function(){
+        if ($scope.hideEqualFiles){
+            $scope.hideEqualFiles=false;
+        }else{
+            $scope.hideEqualFiles=true;
+        }
+    }
+
     $scope.applyChange = function(){
         if (!$scope.sourceDir||!$scope.targetDir){
             alert('Source and Target dir can not be empty')
@@ -76,8 +84,15 @@ fileSyncApp.directive('fileSync', [()=>{
     return {
         template:`<div class='container'>
                     <div class='row'>
-                        <label class='col-lg-6 text-center'>Source Directory</label>
-                        <label class='col-lg-6 text-center'>Target Directory</label>
+                        <label class='col-lg-5 text-center'>Source Directory</label>
+                        <div class='col-lg-2'>
+                            <div class='row'>
+                            <span class='col-lg-1 glyphicon glyphicon-unchecked' ng-click='hideEquals()' ng-show='!hideEqualFiles'/>
+                            <span class='col-lg-1 glyphicon glyphicon-check' ng-click='hideEquals()' ng-show='hideEqualFiles'/>
+                            <span class='col-lg-10'>Hide identitcal files</span>
+                            </div>
+                        </div>
+                        <label class='col-lg-5 text-center'>Target Directory</label>
                     </div>
                     <div class='row'>
                         <input id='sourceDir' ng-model='sourceDir' class='col-lg-5'/>
@@ -85,12 +100,12 @@ fileSyncApp.directive('fileSync', [()=>{
                         <input id='targetDir' ng-model='targetDir' class='col-lg-5'/>
                         <button ng-click='applyChange()' style='position:absolute'>ApplyChange</button>
                     </div>
-                    <div class='row' id="{{diff.file}}" ng-repeat='diff in diffs'>
+                    <div class='row' id="{{diff.file}}" ng-repeat='diff in diffs' ng-show='!hideEqualFiles||diff.direction!=="=="'>
                         <div class='col-lg-5'>
                             <div class='row'>
                                 <span class='col-lg-1 glyphicon glyphicon-file' ng-show='diff.sourceName&&index.diffs[diff.file].type==="FILE"'/>
                                 <span class='col-lg-1 glyphicon glyphicon-folder-open' ng-show='diff.sourceName&&index.diffs[diff.file].type==="DIR"'/>
-                                <span class='col-lg-11' data-toggle="tooltip" title='Size:{{index.diffs[diff.file].sourceSize}}; Last Modified:{{index.diffs[diff.file].sourceTime}}'>{{diff.sourceName}}</span>
+                                <span class='col-lg-11' data-toggle="tooltip" title='Size:{{index.diffs[diff.file].sourceSize}}; Last Modified:{{index.diffs[diff.file].sourceTime| date:"MMM d, y h:mm:ss a Z"}}'>{{diff.sourceName}}</span>
                             </div>
                         </div>
                         
@@ -101,7 +116,7 @@ fileSyncApp.directive('fileSync', [()=>{
                             <div class='row'>
                                 <span class='col-lg-1 glyphicon glyphicon-file' ng-show='diff.targetName&&index.diffs[diff.file].type==="FILE"'/>
                                 <span class='col-lg-1 glyphicon glyphicon-folder-open' ng-show='diff.targetName&&index.diffs[diff.file].type==="DIR"'/>
-                                <span class='col-lg-11' data-toggle="tooltip" title='Size:{{index.diffs[diff.file].targetSize}}; Last Modified:{{index.diffs[diff.file].targetTime}}'>{{diff.targetName}}</span>
+                                <span class='col-lg-11' data-toggle="tooltip" title='Size:{{index.diffs[diff.file].targetSize}}; Last Modified:{{index.diffs[diff.file].targetTime| date:"MMM d, y h:mm:ss a Z"}}>{{diff.targetName}}</span>
                             </div>
                         </div>
                     </div>
