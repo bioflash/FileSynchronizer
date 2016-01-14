7d86f0846d7b3de03701177f4261f9af0a4c7eb4
bioflash <bioflash@gmail.com>
2016-01-14 11:41:36 +0800

    add confirm dialog before applying the change

--- client/main.bundle.js
+++ client/main.bundle.js
@@ -129,13 +129,16 @@
 	        }
 	    }
 
+	    $scope.getSelectedCommands=function(){
+	        return $scope.diffs.filter((item)=>item.selected).map((item)=>{
+	                return {direction: item.direction, command: item.command}
+	        });
+	    }
 	    $scope.applyChange = function(){
 	        if (!$scope.sourceDir||!$scope.targetDir){
 	            alert('Source and Target dir can not be empty')
 	        }else{
-	            var data = $scope.diffs.filter((item)=>item.selected).map((item)=>{
-	                return {direction: item.direction, command: item.command}
-	            });
+	            var data = $scope.getSelectedCommands();
 
 	            applyChangeSvc.apply($scope.sourceDir, $scope.targetDir, data).then(
 	                ()=>alert('Operation finished successfully!'),
@@ -149,6 +152,27 @@
 	fileSyncApp.directive('fileSync', [()=>{
 	    return {
 	        template:`<div class='container'>
+	                    <div id="myModal" class="modal fade" role="dialog">
+	                        <div class="modal-dialog">
+	                            <!-- Modal content-->
+	                            <div class="modal-content">
+	                                <div class="modal-header">
+	                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+	                                    <h4 class="modal-title">Confirming Overwrite</h4>
+	                                </div>
+	                                <div class="modal-body">
+	                                    <header> Confirm that You want to synchronize these two directories with the following commands</header>
+	                                    <div ng-repeat="command in diffs" ng-show='command.selected==true&&command.direction!=="=="' style="border-bottom:1px solid black">
+	                                        {{command.command}}
+	                                    </div>
+	                                </div>
+	                                <div class="modal-footer">
+	                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+	                                    <button type="button" class="btn btn-default" data-dismiss="modal" ng-click="applyChange()">Confirm</button>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
 	                    <div class='row'>
 	                        <label class='col-lg-5 text-center'>Source Directory</label>
 	                        <div class='col-lg-2'>
@@ -164,14 +188,14 @@
 	                        <input id='sourceDir' ng-model='sourceDir' class='col-lg-5'/>
 	                        <button ng-click='compare()' class='col-lg-2' ng-disabled='comparing'>Compare</button>
 	                        <input id='targetDir' ng-model='targetDir' class='col-lg-5'/>
-	                        <button ng-click='applyChange()' style='position:absolute'>ApplyChange</button>
+	                        <button style='position:absolute' data-toggle="modal" data-target="#myModal">ApplyChange</button>
 	                    </div>
 	                    <div class='row' id="{{diff.file}}" ng-repeat='diff in diffs' ng-show='!hideEqualFiles||diff.direction!=="=="' style='box-shadow: 0 0 2px black'>
 	                        <div class='col-lg-5'>
 	                            <div class='row'>
 	                                <span class='col-lg-1 glyphicon glyphicon-file' ng-show='diff.sourceName&&index.diffs[diff.file].type==="FILE"'/>
 	                                <span class='col-lg-1 glyphicon glyphicon-folder-open' ng-show='diff.sourceName&&index.diffs[diff.file].type==="DIR"'/>
-	                                <span class='col-lg-11' data-toggle="tooltip" title='Size:{{index.diffs[diff.file].sourceSize}}; Last Modified:{{index.diffs[diff.file].sourceTime| date:"medium"}}'>{{diff.sourceName.substring(sourceDir.length)}}</span>
+	                                <span class='col-lg-11' data-toggle="tooltip" title='Full Name:{{diff.sourceName}} Size:{{index.diffs[diff.file].sourceSize}}; Last Modified:{{index.diffs[diff.file].sourceTime| date:"medium"}}'>{{diff.sourceName.substring(sourceDir.length)}}</span>
 	                            </div>
 	                        </div>
 	                        
@@ -182,7 +206,7 @@
 	                            <div class='row'>
 	                                <span class='col-lg-1 glyphicon glyphicon-file' ng-show='diff.targetName&&index.diffs[diff.file].type==="FILE"'/>
 	                                <span class='col-lg-1 glyphicon glyphicon-folder-open' ng-show='diff.targetName&&index.diffs[diff.file].type==="DIR"'/>
-	                                <span class='col-lg-11' data-toggle="tooltip" title='Size:{{index.diffs[diff.file].targetSize}}; Last Modified:{{index.diffs[diff.file].targetTime| date:"medium"}}'>{{diff.targetName.substring(targetDir.length)}}</span>
+	                                <span class='col-lg-11' data-toggle="tooltip" title='Full Name:{{diff.targetName}} Size:{{index.diffs[diff.file].targetSize}}; Last Modified:{{index.diffs[diff.file].targetTime| date:"medium"}}'>{{diff.targetName.substring(targetDir.length)}}</span>
 	                            </div>
 	                        </div>
 	                    </div>
